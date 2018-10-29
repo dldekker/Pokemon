b3e88a3bc75114478f8cafda00e722e916f9f0bb
Dekker <d.l.dekker@student.tue.nl>
2018-10-30 00:29:43 +0100

    Enabled debug symbols in the windows build file.

--- build-win.sh
+++ build-win.sh
@@ -2,5 +2,5 @@ gcc pokemon.c player.c battle_queue.c battle.c \
 pokemon_data_list.c item_data_list.c item.c ability.c \
 ability_data_list.c move_data_list.c move.c tiles.c main.c \
 util.c -L./lib -I./include -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lglew32 \
--lopengl32 -opokemon.exe
+-lopengl32 -opokemon.exe -g
 
