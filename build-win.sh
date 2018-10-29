bef2e5630b072fdf3271125df41c3d3042028f82
Dekker <d.l.dekker@student.tue.nl>
2018-10-29 18:22:02 +0100

    Fixed windows build.

--- build-win.sh
+++ build-win.sh
@@ -1,4 +1,6 @@
-gcc pokemon.c player.c battle_queue.c battle.c pokemon_data_list.c item_data_list.c item.c ability.c ability_data_list.c move_data_list.c move.c tiles.c main.c util.c \
--I"include/" \
--L"lib/" -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lglew32 -lopengl32
+gcc pokemon.c player.c battle_queue.c battle.c \
+pokemon_data_list.c item_data_list.c item.c ability.c \
+ability_data_list.c move_data_list.c move.c tiles.c main.c \
+util.c -L./lib -I./include -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lglew32 \
+-lopengl32 -opokemon.exe
 
